cc97617b16b9d206b397171c9d71f9407414d6fb
Ryan cho <ryancho@Ryanui-MacBook-Pro.local>
2018-09-04 14:01:20 +0900

    init

---
+++ qc/lion_cho_old.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>qcontact - QR namecard</title>
+</head>
+
+<body>
+welcome lion.cho page<br>
+	<br>
+	GrowthHacker<br>
+	#Vietnam #Thailand #Developer #Blockchian #BlockchainDeveloper <br>
+	<br>
+Lion Cho<br>
+name Kr 조중현<br>
+Chaineers CEO and Co-Founder<br>
++82 (0)10-3944-4663 (f.0504-246-5004)<br>
++84 90-245-1625 <br>
++66 (0)63-382-7118<br>
+lion.cho@chaineers.io<br>
+In: <a href="https://www.linkedin.com/in/lion-cho/">https://www.linkedin.com/in/lion-cho</a><br>
+KR: B-2F, 50, Jong-ro 1-gil, Jongno-gu, Seoul, Korea, 03142 <br>
+VN1: 268 Lý Thường Kiệt, Phường 14, Quận 10, HCM <br>
+VN2: A07.08 Chung cư Luxcity. Số 528 Quận 7, TP HCM <br>
+<br>
+FB : <a href="https://www.linkedin.com/in/lion-cho/">link</a><br>
+BLOG : <a href="https://blog.naver.com/chowin21">link</a><br>
+YOUTBE : <a href="https://www.youtube.com/channel/UChlcXdPqPKiZdr9oFgR-7Lw?view_as=subscriber">link</a><br>
+BLUE PRINT :  <a href="img1.png">blue_print</a>
+<p>
+Develop Info<br>
+unique link :qr_213123712723212 <br>
+<img src="../qu/qr_213123712723212.png">
+
+</body>
+
+</html>
+
---
+++ qu/213123712723212.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+
+</head>
+
+<body>
+Content of the document...... <br>
+if it doesn't work  <a href ="https://qcontact.org/qc/lion_cho.php">link</a>
+<script>
+// similar behavior as an HTTP redirect
+window.location.replace("https://qcontact.org/qc/lion_cho.php");
+
+// similar behavior as clicking on a link
+window.location.href = "https://qcontact.org/qc/lion_cho.php";
+
+</script>
+
+</body>
+
+</html>
+
